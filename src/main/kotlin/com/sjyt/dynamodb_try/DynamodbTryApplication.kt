5263741bc8e57f8804b18a9a7c4ec3121ab93922
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2024-10-06 10:52:14 +0900

    initial commit
    Signed-off-by: Masatoshi Inoue <masatoshi_inoue_aa@mail.toyota.co.jp>
    Co-authored-by: Masatoshi Inoue <masatoshi_inoue_aa@mail.toyota.co.jp>

---
+++ src/main/kotlin/com/sjyt/dynamodb_try/DynamodbTryApplication.kt
@@ -0,0 +1,11 @@
+package com.sjyt.dynamodb_try
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DynamodbTryApplication
+
+fun main(args: Array<String>) {
+	runApplication<DynamodbTryApplication>(*args)
+}
---
+++ src/test/kotlin/com/sjyt/dynamodb_try/DynamodbTryApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sjyt.dynamodb_try
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DynamodbTryApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
